1dda589e22c251a65061b11b15392b1f99fea1b6
NisargMehta2810 <nisargmehta.1028@gmail.com>
2022-07-24 16:57:28 +0530

    Dicee game

---
+++ js/index.js
@@ -0,0 +1,35 @@
+alert("Hello🤗");
+
+let Player1 = prompt("What's your Name of player1?");
+let Player2 = prompt("What's your Name of player2?");
+
+
+
+
+
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩" + Player1 +  " Wins!";
+}
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩" + Player2 +  " Wins!";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "🚩 Match Tie 🚩";
+}
